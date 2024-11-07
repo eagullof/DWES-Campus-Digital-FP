ec97cb3a2d62adbaa20ac74aa4adcbac2cf55d0c
eagullo <eagullof@fpvirtualaragon.es>
2024-11-07 17:42:35 +0100

    Solucionado fallo de diseño con bootstrap

--- Tutoria10-UD2/Supuesto/edit_task.php
+++ Tutoria10-UD2/Supuesto/edit_task.php
@@ -22,6 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Editar Tarea</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body class="m-2">
     <h1>Editar Tarea</h1>
